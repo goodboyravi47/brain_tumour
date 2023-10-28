94efd1dff66194f7300bd152524300df3c05ed50
chakka-guna-sekhar-venkata-chennaiah <110555361+chakka-guna-sekhar-venkata-chennaiah@users.noreply.github.com>
2023-10-28 22:37:04 +0530

    Update brain.py

--- brain.py
+++ brain.py
@@ -3,9 +3,35 @@ from ultralytics import YOLO
 import numpy as np
 import streamlit as st
 
-st.markdown(f'''
-<h1 align='center'>Brain Tumour Classification</h1>
-''',unsafe_allow_html=True)
+glowing_text_style = '''
+    <style>
+        .glowing-text {
+            font-family: 'Arial Black', sans-serif;
+            font-size: 33px;
+            text-align: center;
+            animation: glowing 2s infinite;
+        }
+        
+        @keyframes glowing {
+            0% { color: #FF9933; } /* Saffron color */
+            10% { color: #FFD700; } /* Gold color */
+            20% { color: #FF1493; } /* Deep Pink */
+            30% { color: #00FF00; } /* Lime Green */
+            40% { color: #FF4500; } /* Orange Red */
+            50% { color: #9400D3; } /* Dark Violet */
+            60% { color: #00BFFF; } /* Deep Sky Blue */
+            70% { color: #FF69B4; } /* Hot Pink */
+            80% { color: #ADFF2F; } /* Green Yellow */
+            90% { color: #1E90FF; } /* Dodger Blue */
+            100% { color: #FF9933; } /* Saffron color */
+        }
+    </style>
+'''
+
+
+st.markdown(glowing_text_style, unsafe_allow_html=True)
+st.markdown(f'<p class="glowing-text">Brain Tumour Classification</p>', unsafe_allow_html=True)
+
 @st.cache_resource()
 def finalised_model():
 	model=YOLO('best.pt')
